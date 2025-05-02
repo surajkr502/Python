f705420e49c26721fe161878e849927510f034b3
surajkr502 <sawsurajkumar502@gmail.com>
2025-05-02 19:35:00 +0530

    new update

---
+++ module.py
@@ -0,0 +1,9 @@
+import pyjokes
+print("printing jokes....")
+# This is comments (ctrl+/) single line comments
+joke=pyjokes.get_joke()
+print(joke)
+"""multi line comments
+multi line comments
+"""
+
